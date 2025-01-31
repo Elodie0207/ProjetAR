74860fcde78349531b66a91d703e79f074894c9c
Elodie0207 <elodieguldas@gmail.com>
2025-01-31 20:26:16 +0100

    AjoutBoutonStartLumineux

---
+++ ProjetARmulti/Assets/Scripts/Script Enigme/SignauxLumineux.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignauxLumineux : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    public void OnMouseDown()
+    {
+        Debug.Log("Cube cliqué !");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
